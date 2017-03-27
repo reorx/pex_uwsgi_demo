562cfd29294660920101d8bddde500a39dda6e30
reorx <novoreorx@gmail.com>
2017-03-27 22:47:25 +0800

    trim

--- loadpex.py
+++ loadpex.py
@@ -10,24 +10,17 @@ else:
     from imp import reload
 
 
-#try:
-#    print 'try import amod'
-#    import amod
-#except ImportError:
-#    print 'could not import amod at first'
-#
-
 def activate_pex():
-    sys.stderr.write('[pex_uwsgi] bootstrapping..\n')
+    sys.stderr.write('[loadpex] bootstrapping..\n')
 
     entry_point = os.environ.get('PEX_ENTRY')
     if not entry_point:
         sys.stderr.write(
-            '[pex_uwsgi] couldnt determine pex from PEX_ENTRY environment variable, bailing!\n'
+            '[loadpex] couldnt determine pex from PEX_ENTRY environment variable, bailing!\n'
         )
         sys.exit(1)
 
-    sys.stderr.write('[pex_uwsgi] entry_point=%s\n' % entry_point)
+    sys.stderr.write('[loadpex] entry_point=%s\n' % entry_point)
 
     sys.path[0] = os.path.abspath(sys.path[0])
     sys.path.insert(0, entry_point)
@@ -41,11 +34,8 @@ def activate_pex():
     import pkg_resources
     reload(pkg_resources)
 
-    sys.stderr.write('[pex_uwsgi] sys.path=%s\n\n' % sys.path)
+    sys.stderr.write('[loadpex] sys.path=%s\n\n' % sys.path)
     return
 
 
 activate_pex()
-
-#print 'activated, import amod'
-#import amod
